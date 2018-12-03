b929fdaa87448370421f96fd639e29c65314dd4e
preyank <preyank@ccs.neu.edu>
2018-12-02 19:14:45 -0500

    relevance feedback

---
+++ Task2/BM25WithFeedback.py
@@ -0,0 +1,89 @@
+from Helper import Helper
+from PsuedoRelevanceFeedBack import PsuedoRelevanceFeedBack
+from collections import defaultdict
+import math
+import operator
+
+
+class BM25WithFeedback:
+    def __init__(self, k1,k2, b):
+        self.helper = Helper()
+        self.k1 = k1
+        self.k2 = k2
+        self.b = b
+        self.calcAVDL()
+        self.queryFrequencyDict = defaultdict(int)
+        self.docScoreDict = defaultdict(int)
+        self.main()
+
+
+    def initializeDocScoreDict(self):
+        for key in self.helper.number_of_terms_doc.keys():
+            self.docScoreDict[key] = 0
+
+    def calculateK(self, doc_id):
+        retVal = self.k1*((1-self.b) + self.b*(self.helper.number_of_terms_doc[doc_id]/self.avdl))
+        return retVal
+
+    def calcAVDL(self):
+        sum = 0
+        for key in self.helper.number_of_terms_doc.keys():
+            sum += self.helper.number_of_terms_doc[key]
+            self.avdl = sum/len(self.helper.number_of_terms_doc.keys())
+
+    def calculateDocumentScore(self,term, doc_id, qId):
+        r = 0
+        R = 0
+        n = len(self.helper.unigram_inverted_index[term].keys())
+        N = len(self.helper.number_of_terms_doc.keys())
+        K = self.calculateK(doc_id)
+        f = self.helper.unigram_inverted_index[term][doc_id]
+        qf = self.queryFrequencyDict[term]
+        docScore = \
+            math.log(((r + 0.5)/(R - r + 0.5))/((n-r+0.5)/(N - n - R + r +0.5))
+                     *(((self.k1+1)*f)/(K + f))
+                     *(((self.k2 + 1)*qf)/(self.k2 + qf)))
+
+        self.docScoreDict[doc_id] += docScore
+
+
+    def createQueryFrequencyDict(self, queryAr):
+        for term in queryAr:
+            if term in self.queryFrequencyDict.keys():
+                self.queryFrequencyDict[term] += 1
+            else:
+                self.queryFrequencyDict[term] = 1
+
+
+    def calculateTermScore(self, term, qId):
+        for doc_id in self.helper.unigram_inverted_index[term]:
+            self.calculateDocumentScore(term, doc_id, qId)
+
+
+    def score(self, qId, queryAr):
+        for term in queryAr:
+            self.calculateTermScore(term, qId)
+
+    def printScores(self, qId):
+        sortedDict = sorted(self.docScoreDict.items(), key=operator.itemgetter(1), reverse=True)
+        file = open("BM_OutputWithFeedback/"+str(qId)+"-withFeedbackScore.txt", "w")
+        rank = 0
+        for tup in sortedDict:
+            rank += 1
+            file.write(str(tup[0]) + " " + str(tup[1]) + " \n")
+            if rank == 100:
+                break
+
+    def main(self):
+        queries = self.helper.get_queries()
+        for q in queries.keys():
+            self.query = queries[q]
+            self.query = self.helper.parse_query(self.query)
+            queryAr = self.query.split()
+            psuedoRelevanceFeedBack = PsuedoRelevanceFeedBack()
+            queryAr.extend(psuedoRelevanceFeedBack.main(q, queryAr))
+            self.queryFrequencyDict = defaultdict(int)
+            self.docScoreDict = defaultdict(int)
+            self.createQueryFrequencyDict(queryAr)
+            self.score(q, queryAr)
+            self.printScores(q)
---
+++ Task2/PsuedoRelevanceFeedBack.py
@@ -0,0 +1,94 @@
+from Helper import Helper
+from collections import defaultdict
+import math
+import operator
+
+
+class PsuedoRelevanceFeedBack:
+
+    def prepare_stoplist(self):
+        with open(self.stopwords_file, 'r') as f:
+            text_data = f.read().split()
+            self.stop_words = text_data
+
+    def initRelavantDocVector(self, qId):
+        with open('BM_Output/'+str(qId)+'-score.txt', 'r') as f:
+            i = 1
+            lines = f.readlines()
+            for line in lines:
+                i += 1
+                ar = line.split()
+                self.documnetVector[ar[0]] = {};
+                if i == 12:
+                    break
+
+    def populateRelevantDocVector(self):
+        for term in self.helper.unigram_inverted_index.keys():
+            for docId in self.helper.unigram_inverted_index[term].keys():
+                self.documnetVector[docId][term] = self.helper.unigram_inverted_index[term][docId]
+
+
+    def getQueryVector(self):
+        for term in self.query.split():
+            if term in self.queryVector.keys():
+                self.queryVector[term] += 1
+            else:
+                self.queryVector[term] = 1
+
+        for t in self.helper.unigram_inverted_index.keys():
+            if t not in self.queryVector.keys():
+                self.queryVector[t] = 0
+
+    def calculateVectorScore(self, term):
+        retVal = 0;
+        for docId in self.documnetVector.keys():
+            if term in self.documnetVector[docId].keys():
+                retVal += self.documnetVector[docId][term]
+        return retVal
+
+
+
+    def calculateRocchioScore(self, qId, queryAr):
+        expansionTerms = []
+        a = 4
+        b = 16
+        c = 0
+        for t in self.helper.unigram_inverted_index.keys():
+            self.queryVector[t] = a*self.queryVector[t] + b*self.calculateVectorScore(t)
+
+        sortedDict = sorted(self.queryVector.items(), key=operator.itemgetter(1), reverse=True)
+
+        i = 0
+        for tup in sortedDict:
+            #if tup[0] not in self.stop_words:
+            i += 1
+            if tup[0] not in queryAr:
+                expansionTerms.append(tup[0])
+                print(str(tup[0]))
+            if i == 50:
+                break
+
+        with open('ExpansionTerms/' + str(qId) + '-ExpTerms.txt', 'w') as f:
+            for term in expansionTerms:
+                f.write(term+"\n")
+        return expansionTerms
+
+    def main(self, qId, queryAr):
+        self.helper = Helper()
+        self.documnetVector = defaultdict(dict)
+        self.queryVector = defaultdict(str)
+        self.docScoreDict = defaultdict(int)
+        self.stopwords_file = "../test-collection/common_words"
+        self.output_dir = "./stopped_pages_output/"
+        self.prepare_stoplist()
+        self.stopwords_file = "../test-collection/common_words"
+        self.prepare_stoplist()
+        queries = self.helper.get_queries()
+        self.query = queries[qId]
+        self.documnetVector = defaultdict(dict)
+        self.queryVector = defaultdict(str)
+        self.getQueryVector()
+        self.initRelavantDocVector(qId)
+        self.populateRelevantDocVector()
+        return self.calculateRocchioScore(qId, queryAr)
+
