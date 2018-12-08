6277c940947f4a9d6a40ca908ccbfc2cb432fc1c
preyank <preyank@ccs.neu.edu>
2018-12-08 01:13:41 -0500

    snippet generation

---
+++ Snippet generation/Helper.py
@@ -0,0 +1,76 @@
+import math
+from collections import defaultdict
+import re
+import operator
+from bs4 import BeautifulSoup
+
+class Helper:
+    def __init__(self):
+        self.unigram_inverted_index = defaultdict(dict)
+        self.number_of_terms_doc = defaultdict(int)
+        self.total_number_of_terms_corpus = 0
+        self.create_inverted_index()
+        self.create_number_of_terms_doc_dict()
+
+    def create_inverted_index(self):
+        with open('unigram_index.txt', 'r') as f:
+            for line in f.read().splitlines():
+                line_list = line.split()
+                term = line_list.pop(0)
+                term = term.replace(':', '')
+                for element in line_list:
+                    try:
+                        element = element.replace('(', '')
+                        element = element.replace(')', '')
+                        doc_id = element.split(':')[0]
+                        tf_doc = int(element.split(':')[1].strip())
+                        self.unigram_inverted_index[term][doc_id] = tf_doc
+                    except IndexError as e:
+                        print (line_list)
+                        print (element)
+
+    def create_number_of_terms_doc_dict(self):
+        with open('number_of_terms_unigram.txt', 'r') as f:
+            for line in f.read().splitlines():
+                doc_id = line.split(':')[0]
+                number_of_terms = int(line.split(':')[1].strip())
+                self.number_of_terms_doc[doc_id] = number_of_terms
+                self.total_number_of_terms_corpus += number_of_terms
+        #print self.number_of_terms_doc
+
+    def corpus_frequency(self,unigram_inverted_index):
+        corpus_term_count_dictionary = {}
+        for key1 in unigram_inverted_index.keys():
+            corpus_term_count_dictionary[key1] = 0
+            for key2 in unigram_inverted_index[key1].keys():
+                corpus_term_count_dictionary[key1] += unigram_inverted_index[key1][key2]
+
+        return corpus_term_count_dictionary
+
+    def parse_query(self,query):
+        query = query.lower() 
+        regex = r"(?!\d)[.,;](?!\d)"
+        regex2 = r"[(){}\"#~\[\]<>=:?!@&'|*]"
+        regex3 = r"(?!\d|\w)[-/$](?!\d|\w)"
+        query = re.sub(regex, "", query, 0)
+        query = re.sub(regex2, "", query, 0)
+        query = re.sub(regex3, "", query, 0)
+        return query
+
+    def get_queries(self):
+        queries = {}
+        with open('../test-collection/cacm.query.txt', 'r') as f:
+            raw_data = f.read()
+            bs = BeautifulSoup(raw_data, 'html.parser') 
+            docs = bs.find_all('doc')
+            for doc in docs:
+                doc_text = doc.get_text()
+                lines = doc_text.replace("\n", ' ')
+                lines = lines.split("\n")
+                for line in lines:
+                    line_list = line.split()
+                    query_id  = int(line_list.pop(0))
+                    query = ' '.join(line_list)
+                    queries[query_id] = query
+        return queries                
+                
--- Snippet generation/SnippetGeneration.py
+++ Snippet generation/SnippetGeneration.py
@@ -1,19 +1,20 @@
 import math
 from collections import defaultdict
+from Helper import *
 import re
 import operator
 from bs4 import BeautifulSoup
 
 class SnippetGeneration:
-    def __init__(self, q):
-        self.query = "What articles exist which deal with TSS (Time Sharing System), an operating system for IBM computers?"
+    def __init__(self):
+        self.query = []
+        self.helper = Helper()
 
 
 
-    def create_term_array_dict(self):
+    def create_term_array_dict(self,qId):
         term_array_dict = {}
-        #for i in range(1, 65):
-        with open('BM_Output/'+str(1)+'-score.txt', 'r') as f:
+        with open('BM_Output/'+str(qId)+'-score.txt', 'r') as f:
             lines = f.readlines()
             for line in lines:
                 ar = line.split()
@@ -22,10 +23,10 @@ class SnippetGeneration:
                     term_array_dict[ar[0]] = reader.split()
         return term_array_dict
 
-    def createSnippetDict(self):
+    def createSnippetDict(self, qId):
         snippetList = []
         term_array_dict = {}
-        term_array_dict = self.create_term_array_dict()
+        term_array_dict = self.create_term_array_dict(qId)
         snippetDict = {}
         for key in term_array_dict.keys():
             i = 0;
@@ -38,31 +39,44 @@ class SnippetGeneration:
                     else:
                         i = 0
                 else:
+                    i = 0
                     snippetList.append(snippet)
+                    snippet = []
             snippetDict[key] = snippetList
-
+            snippetList = []
         return snippetDict
 
-    def calcScore(self):
+    def calcScore(self,qId):
         finalSnippetDict = {}
         snippetDict = {}
-        snippetDict = self.createSnippetDict()
+        snippetDict = self.createSnippetDict(qId)
         for key in snippetDict.keys():
-            for snippetList in snippetDict[key]:
-                maxScore = 0
-                maxSnippet = snippetList[0]
-                for snippet in snippetList:
-                    score = 0
-                    for term in snippet:
-                        if term in self.query:
-                            score += 1
-                    if maxScore < score:
-                        maxScore = score
-                        maxSnippet = snippet
-                finalSnippetDict[key] = maxSnippet
-        for key in finalSnippetDict.keys():
-            print(finalSnippetDict[key])
+            maxScore = 0
+            maxSnippet = snippetDict[key][0]
+            for snippet in snippetDict[key]:
+                score = 0
+                for term in snippet:
+                    if term in self.query:
+                        score += 1
+                if maxScore < score:
+                    maxScore = score
+                    maxSnippet = snippet
+            finalSnippetDict[key] = maxSnippet
+
+        with open('snippet_output/'+str(qId)+'-snippet.txt', 'w') as f:
+            for key in finalSnippetDict.keys():
+                f.write(key+" : ")
+                for term in finalSnippetDict[key]:
+                    if term in self.query:
+                        f.write("<b>"+ term + "</b> ")
+                    else:
+                        f.write(term + " ")
+                f.write("\n")
 
 
-
+    def main(self):
+        queries = self.helper.get_queries();
+        for qId in queries.keys():
+            self.query = queries[qId].split()
+            self.calcScore(qId)
 
